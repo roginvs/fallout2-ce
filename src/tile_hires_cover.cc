6541eb7bee323e4563de64117ea932baf1a37383
Vasilii Rogin <vasya@rogin.ru>
2024-11-12 20:46:31 +0200

    WIP hires cover

--- src/tile_hires_cover.cc
+++ src/tile_hires_cover.cc
@@ -4,54 +4,39 @@
 #include <string.h>
 namespace fallout {
 
-// TODO: Use square screen tiles 16x18 in size
-// And have size 500x200 elements
-// Keep this as bool for traversal
-static unsigned char tiles[ELEVATION_COUNT][HEX_GRID_SIZE];
+static bool visited_tiles[ELEVATION_COUNT][HEX_GRID_SIZE];
 
-// TODO: Remove this, use boolean, and clean tiles if center is not traversed
-static unsigned char view_area_id = 1;
+static constexpr int square_width = 16;
+static constexpr int square_height = 12;
+static constexpr int square_grid_width = 500;
+static constexpr int square_grid_height = 300;
+static bool visible_squares[ELEVATION_COUNT][square_grid_width][square_grid_height];
+static_assert(square_width * square_grid_width == 8000);
+static_assert(square_height * square_grid_height == 3600);
+
+static constexpr int screen_view_width = 640;
+static constexpr int screen_view_height = 380;
+
+static constexpr int squares_screen_width_half = screen_view_width / 2 / square_width;
+static_assert(screen_view_width % (2 * square_width) == 0);
+static constexpr int squares_screen_height_half = screen_view_height / 2 / square_height;
+// static_assert(screen_view_height % (2 * square_height) == 0);
 
 #define CENTER_VISITED_FLAG 0b10000000
 // #define TILE_HALF_VISIBLE 0b01000000
 #define CLEAR_FLAGS 0b00111111
+
+// fallout2-ce.js:1816 min tileX=199 x=-3312 tileY=0 y=-1390 maxX=4656 maxY=2186 dx=7968 dy=3576
+static constexpr int hex_tile_with_lowest_x = 199;
+static constexpr int hex_tile_with_lowest_y = 0;
 
 void init_tile_hires()
 {
     printf("===========================\n");
     printf("init_tile_hires\n");
     printf("===========================\n");
-    memset(tiles, 0, sizeof(tiles));
-    view_area_id = 1;
-
-    {
-        int min_x = 0;
-        int min_y = 0;
-        int max_x = 0;
-        int max_y = 0;
-        int x;
-        int y;
-        for (int i = 0; i < HEX_GRID_SIZE; i++) {
-            tileToScreenXY(i, &x, &y, gElevation);
-            if (x < min_x) {
-                min_x = x;
-            }
-            if (y < min_y) {
-                min_y = y;
-            }
-            if (x > max_x) {
-                max_x = x;
-            }
-            if (y > max_y) {
-                max_y = y;
-            }
-        };
-        printf("min_x=%i min_y=%i max_x=%i max_y=%i\n", min_x, min_y, max_x, max_y);
-        printf("screen width=%i height=%i\n", max_x - min_x, max_y - min_y);
-        printf("screen with tile size width=%i height=%i\n", max_x - min_x + 32, max_y - min_y + 18);
-        // screen width=7968 height=3576
-        // screen with tile size width=8000 height=3594
-    }
+    memset(visited_tiles, 0, sizeof(visited_tiles));
+    memset(visible_squares, 0, sizeof(visible_squares));
 }
 
 /*
@@ -80,58 +65,114 @@ horizontal 20
 
 24 px is vertical distance between 2 rows (up and down arrow press)
 
+
+
+View is 640 x 380
 */
+
+struct XY {
+    int x;
+    int y;
+};
+
+#define DO_DEBUG_CHECKS true
+
+#if DO_DEBUG_CHECKS == true
+#include <stdlib.h>
+
+#endif
+
+static struct XY get_screen_diff()
+{
+    if (DO_DEBUG_CHECKS) {
+        int minX = 0xFFFFFFFF;
+        int minY = 0xFFFFFFFF;
+        int maxX = 0;
+        int maxY = 0;
+        int minTileX = -1;
+        int minTileY = -1;
+        for (int t = 0; t < HEX_GRID_SIZE; t++) {
+            int x;
+            int y;
+            tileToScreenXY(t, &x, &y, gElevation);
+            if (x < minX) {
+                minX = x;
+                minTileX = t;
+            };
+            if (y < minY) {
+                minY = y;
+                minTileY = t;
+            };
+            if (x > maxX) {
+                maxX = x;
+            }
+            if (y > maxY) {
+                maxY = y;
+            }
+        }
+        if (minTileX != hex_tile_with_lowest_x || minTileY != hex_tile_with_lowest_y) {
+            printf("min tileX=%i x=%i tileY=%i y=%i maxX=%i maxY=%i dx=%i dy=%i\n",
+                minTileX, minX,
+                minTileY, minY,
+                maxX, maxY,
+                maxX - minX, maxY - minY);
+            exit(100);
+        }
+    };
+
+    int offsetX;
+    int tmp;
+    int offsetY;
+    tileToScreenXY(hex_tile_with_lowest_x, &offsetX, &tmp, gElevation);
+    tileToScreenXY(hex_tile_with_lowest_y, &tmp, &offsetY, gElevation);
+    return {
+        .x = offsetX,
+        .y = offsetY,
+    };
+};
 
 static void mark_screen_tiles_around_as_visible(int center_tile)
 {
-    int c1 = 0;
-    int c2 = 0;
 
     int centerTileScreenX;
     int centerTileScreenY;
     tileToScreenXY(center_tile, &centerTileScreenX, &centerTileScreenY, gElevation);
+    auto screen_diff = get_screen_diff();
 
-    constexpr int tileWidth = 32;
-    constexpr int tileDoubleHeight = 24;
+    int centerTileGlobalX = centerTileScreenX - screen_diff.x;
+    int centerTileGlobalY = centerTileScreenY - screen_diff.y;
 
-    constexpr int tilesOnScreenX = 18; // 20;
-    constexpr int tilesOnScreenY = 30; // 32;
+    if (DO_DEBUG_CHECKS) {
+        if (centerTileGlobalX % square_width != 0 || centerTileGlobalY % square_height != 0) {
+            printf("centerTileGlobalX=%i, centerTileGlobalY=%i\n", centerTileGlobalX, centerTileGlobalY);
+            exit(100);
+        }
 
-    for (int x = centerTileScreenX - tileWidth * tilesOnScreenX / 2;
-         x <= centerTileScreenX + tileWidth * tilesOnScreenX / 2;
-         x += tileWidth) {
-        for (int y = centerTileScreenY - tileDoubleHeight * tilesOnScreenY / 4;
-             y <= centerTileScreenY + tileDoubleHeight * tilesOnScreenY / 4; y += tileDoubleHeight) {
-            int tile = tileFromScreenXY(x, y, gElevation, true);
-            if (tile != -1 && tiles[gElevation][tile] == 0) {
-                tiles[gElevation][tile] = view_area_id;
-                c1++;
-            }
+        if (
+            centerTileGlobalX - squares_screen_width_half * square_width < 0 || centerTileGlobalY - squares_screen_height_half * square_height < 0) {
+            printf("centerTileGlobalX=%i, centerTileGlobalY=%i\n", centerTileGlobalX, centerTileGlobalY);
+            exit(100);
+        }
+        if (
+            centerTileGlobalX + squares_screen_width_half * square_width >= square_width * square_grid_width
+            || centerTileGlobalY + squares_screen_height_half * square_height >= square_height * square_grid_height) {
+            printf("centerTileGlobalX=%i, centerTileGlobalY=%i\n", centerTileGlobalX, centerTileGlobalY);
+            exit(100);
         }
     }
 
-    centerTileScreenX += tileWidth / 2;
-    centerTileScreenY += tileDoubleHeight / 2;
-
-    for (int x = centerTileScreenX - tileWidth * tilesOnScreenX / 2;
-         x <= centerTileScreenX + tileWidth * tilesOnScreenX / 2;
-         x += tileWidth) {
-        for (int y = centerTileScreenY - tileDoubleHeight * tilesOnScreenY / 4;
-             y <= centerTileScreenY + tileDoubleHeight * tilesOnScreenY / 4; y += tileDoubleHeight) {
-            int tile = tileFromScreenXY(x, y, gElevation, true);
-            if (tile != -1 && tiles[gElevation][tile] == 0) {
-                tiles[gElevation][tile] = view_area_id;
-                c2++;
-            }
+    int squareX = centerTileGlobalX / square_width;
+    int squareY = centerTileGlobalY / square_height;
+    for (int x = squareX - squares_screen_width_half; x <= squareX + squares_screen_width_half; x++) {
+        for (int y = squareY - squares_screen_height_half; y <= squareY + squares_screen_height_half; y++) {
+            visible_squares[gElevation][x][y] = true;
         }
     }
-
-    printf("Done visibility id=%i c1=%i c2=%i\n", view_area_id, c1, c2);
 }
 
 void on_center_tile_change()
 {
-    if (tiles[gElevation][gCenterTile] != 0) {
+    if (visited_tiles[gElevation][gCenterTile]) {
         printf("========= NOOP on_center_tile_change elev=%i tile=%i ================\n",
             gElevation, gCenterTile);
 
@@ -140,64 +181,64 @@ void on_center_tile_change()
     printf("=========== on_center_tile_change elev=%i tile=%i ================\n",
         gElevation, gCenterTile);
 
+    // TODO: Clear only current elevation
+    init_tile_hires();
+
     std::stack<int> tiles_to_visit;
     tiles_to_visit.push(gCenterTile);
 
-    int c = 0;
+    int visited_tiles_count = 0;
     while (!tiles_to_visit.empty()) {
         auto tile = tiles_to_visit.top();
         tiles_to_visit.pop();
 
-        if (tile < 0) {
+        if (visited_tiles[gElevation][tile]) {
             continue;
         }
 
-        if (tiles[gElevation][tile] & CENTER_VISITED_FLAG) {
-            continue;
-        }
+        visited_tiles_count++;
 
-        c++;
-
-        tiles[gElevation][tile] = view_area_id | CENTER_VISITED_FLAG;
+        visited_tiles[gElevation][tile] = true;
         mark_screen_tiles_around_as_visible(tile);
+
+        break;
 
         int tileScreenX;
         int tileScreenY;
         tileToScreenXY(tile, &tileScreenX, &tileScreenY, gElevation);
 
-        // TODO: Check are there scroll blocker!
-        /*
-                tiles_to_visit.push(
-                    tileFromScreenXY(tileScreenX - 32, tileScreenY, gElevation, true));
-                tiles_to_visit.push(
-                    tileFromScreenXY(tileScreenX + 32, tileScreenY, gElevation, true));
-                tiles_to_visit.push(
-                    tileFromScreenXY(tileScreenX, tileScreenY - 24, gElevation, true));
-                tiles_to_visit.push(
-                    tileFromScreenXY(tileScreenX, tileScreenY + 24, gElevation, true));
-                    */
+        {
+            int neighbor = tileFromScreenXY(tileScreenX - 32, tileScreenY, gElevation, true);
+            if (neighbor >= 0 && neighbor <= HEX_GRID_SIZE && _obj_scroll_blocking_at(neighbor, gElevation) == 0) {
+                tiles_to_visit.push(neighbor);
+            }
+        }
+        {
+            int neighbor = tileFromScreenXY(tileScreenX + 32, tileScreenY, gElevation, true);
+            if (neighbor >= 0 && neighbor <= HEX_GRID_SIZE && _obj_scroll_blocking_at(neighbor, gElevation) == 0) {
+                tiles_to_visit.push(neighbor);
+            }
+        }
+        {
+            int neighbor = tileFromScreenXY(tileScreenX, tileScreenY - 24, gElevation, true);
+            if (neighbor >= 0 && neighbor <= HEX_GRID_SIZE && _obj_scroll_blocking_at(neighbor, gElevation) == 0) {
+                tiles_to_visit.push(neighbor);
+            }
+        }
+        {
+            int neighbor = tileFromScreenXY(tileScreenX, tileScreenY + 24, gElevation, true);
+            if (neighbor >= 0 && neighbor <= HEX_GRID_SIZE && _obj_scroll_blocking_at(neighbor, gElevation) == 0) {
+                tiles_to_visit.push(neighbor);
+            }
+        }
+
+        if (visited_tiles_count > 10) {
+            // This is for debug
+            break;
+        }
     }
 
-    printf("Done center traversal id=%i c=%i\n", view_area_id, c);
-
-    // view_area++;
-    /*
-     tiles[gElevation][gCenterTile] = 1;
-     if (gCenterTile > 0) {
-         tiles[gElevation][gCenterTile - 1] = 2;
-     };
-
-     if (gCenterTile < HEX_GRID_SIZE - 1) {
-         tiles[gElevation][gCenterTile + 1] = 2;
-     };
-     if (gCenterTile >= HEX_GRID_HEIGHT) {
-         tiles[gElevation][gCenterTile - HEX_GRID_HEIGHT] = 2;
-     };
-     if (gCenterTile < HEX_GRID_SIZE - HEX_GRID_HEIGHT) {
-         tiles[gElevation][gCenterTile + HEX_GRID_HEIGHT] = 2;
-     };
- */
-    // _obj_scroll_blocking_at
+    printf("Done center traversal visited_tiles_count=%i\n", visited_tiles_count);
 }
 
 /*
@@ -242,38 +283,40 @@ void draw_tile_hires_cover(Rect* rect, unsigned char* buffer, int windowWidth, i
     int maxX = updatedRect.right;
     int maxY = updatedRect.bottom;
 
-    int leftTop = tileFromScreenXY(minX, minY, gElevation, true);
-    int rightTop = tileFromScreenXY(maxX, minY, gElevation, true);
-    int leftBottom = tileFromScreenXY(minX, maxY, gElevation, true);
-    int rightBottom = tileFromScreenXY(maxX, maxY, gElevation, true);
+    /*
+        int leftTop = tileFromScreenXY(minX, minY, gElevation, true);
+        int rightTop = tileFromScreenXY(maxX, minY, gElevation, true);
+        int leftBottom = tileFromScreenXY(minX, maxY, gElevation, true);
+        int rightBottom = tileFromScreenXY(maxX, maxY, gElevation, true);
 
-    // TODO: Use rect instead of going through all tiles
-    for (int i = 0; i < HEX_GRID_SIZE; i++) {
-        if (tiles[gElevation][i] != 0) {
-            // TODO Check if centerTile have the same view_area
-            continue;
-        }
-        int color = 0x40;
-
-        int screenX;
-        int screenY;
-        tileToScreenXY(i, &screenX, &screenY, gElevation);
-        constexpr int tileWidth = 32;
-        constexpr int tileHeight = 18;
-
-        if (screenX < 0 || screenY < 0 || screenX + tileWidth >= windowWidth || screenY + tileHeight >= windowHeight) {
-            // TODO: Instead of skipping, draw only visible part
-            continue;
-        };
-
-        int pixel = screenY * windowWidth + screenX;
-        for (int y = 0; y < tileHeight; y++) {
-            for (int x = 0; x < tileWidth; x++) {
-                buffer[pixel + x] = color;
+        // TODO: Use rect instead of going through all tiles
+        for (int i = 0; i < HEX_GRID_SIZE; i++) {
+            if (tiles[gElevation][i] != 0) {
+                // TODO Check if centerTile have the same view_area
+                continue;
             }
-            pixel += windowWidth;
+            int color = 0x40;
+
+            int screenX;
+            int screenY;
+            tileToScreenXY(i, &screenX, &screenY, gElevation);
+            constexpr int tileWidth = 32;
+            constexpr int tileHeight = 18;
+
+            if (screenX < 0 || screenY < 0 || screenX + tileWidth >= windowWidth || screenY + tileHeight >= windowHeight) {
+                // TODO: Instead of skipping, draw only visible part
+                continue;
+            };
+
+            int pixel = screenY * windowWidth + screenX;
+            for (int y = 0; y < tileHeight; y++) {
+                for (int x = 0; x < tileWidth; x++) {
+                    buffer[pixel + x] = color;
+                }
+                pixel += windowWidth;
+            }
         }
-    }
+        */
 }
 
 } // namespace fallout
