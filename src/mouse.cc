d2738a7521482183ccbe34ed5b0d0979670cc5b7
Vasilii Rogin <vasya@rogin.ru>
2025-03-05 21:14:17 +0200

    Revert original coefficients for two finger scroll

--- src/mouse.cc
+++ src/mouse.cc
@@ -411,8 +411,8 @@ void _mouse_info()
                 if (!touch_get_pan_mode() && gesture.numberOfTouches == 1) {
                     _mouse_simulate_input(gesture.x - prevx, gesture.y - prevy, 0);
                 } else if (touch_get_pan_mode() || gesture.numberOfTouches == 2) {
-                    gMouseWheelX = (prevx - gesture.x) / 8;
-                    gMouseWheelY = (gesture.y - prevy) / 8;
+                    gMouseWheelX = (prevx - gesture.x) / 2;
+                    gMouseWheelY = (gesture.y - prevy) / 2;
 
                     if (gMouseWheelX != 0 || gMouseWheelY != 0) {
                         gMouseEvent |= MOUSE_EVENT_WHEEL;
