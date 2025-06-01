aed05c0daf0aa0a715a9d9835a84a4ae78d883e9
Vasilii Rogin <vasya@rogin.ru>
2025-06-01 16:10:12 +0300

    Initial implemenation of sprintf

--- src/sfall_metarules.cc
+++ src/sfall_metarules.cc
@@ -1,5 +1,6 @@
 #include "sfall_metarules.h"
 
+#include <algorithm>
 #include <string.h>
 
 #include "combat.h"
@@ -46,6 +47,7 @@ static void mf_set_ini_setting(Program* program, int args);
 static void mf_set_outline(Program* program, int args);
 static void mf_show_window(Program* program, int args);
 static void mf_tile_refresh_display(Program* program, int args);
+static void mf_string_format(Program* program, int args);
 
 constexpr MetaruleInfo kMetarules[] = {
     { "car_gas_amount", mf_car_gas_amount, 0, 0 },
@@ -66,6 +68,7 @@ constexpr MetaruleInfo kMetarules[] = {
     { "set_outline", mf_set_outline, 2, 2 },
     { "show_window", mf_show_window, 0, 1 },
     { "tile_refresh_display", mf_tile_refresh_display, 0, 0 },
+    { "string_format", mf_string_format, 2, 8 },
 };
 
 constexpr int kMetarulesMax = sizeof(kMetarules) / sizeof(kMetarules[0]);
@@ -253,6 +256,119 @@ void mf_tile_refresh_display(Program* program, int args)
 {
     tileWindowRefresh();
     programStackPushInteger(program, -1);
+}
+
+void mf_string_format(Program* program, int args)
+{
+    sprintf_lite(program, args, "string_format");
+}
+
+void sprintf_lite(Program* program, int args, const char* infoOpcodeName)
+{
+    auto format = programStackPopString(program); // Pop the format string
+
+    ProgramValue formatArgs[7]; // 8 arguments total, 1 for format string
+
+    for (int index = 0; index < args - 1; index++) {
+        formatArgs[index] = programStackPopValue(program);
+    }
+
+    int fmtLen = strlen(format);
+    if (fmtLen == 0) {
+        programStackPushString(program, "");
+        return;
+    }
+    if (fmtLen > 1024) {
+        debugPrint("%s(): format string exceeds maximum length of 1024 characters.", infoOpcodeName);
+        programStackPushString(program, "Error");
+        return;
+    }
+    int newFmtLen = fmtLen;
+
+    for (int i = 0; i < fmtLen; i++) {
+        if (format[i] == '%') newFmtLen++; // will possibly be escaped, need space for that
+    }
+
+    // parse format to make it safe
+    char* newFmt = new char[newFmtLen + 1];
+    bool conversion = false;
+    int j = 0;
+    int valIdx = 0;
+
+    char out[5120] = { 0 };
+
+    char* outBuf = out;
+    int bufCount = sizeof(outBuf) - 1;
+
+    int numArgs = args; // From 2 to 8
+
+    for (int i = 0; i < fmtLen; i++) {
+        char c = format[i];
+        if (!conversion) {
+            // Start conversion.
+            if (c == '%') conversion = true;
+        } else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '%') {
+            int partLen;
+            if (c == '%') {
+                // escaped % sign, just copy newFmt up to (and including) the leading % sign
+                newFmt[j] = '\0';
+                // strncpy_s(outBuf, bufCount, newFmt, j);
+                strncpy(outBuf, newFmt, std::min(j, bufCount - 1));
+                partLen = j;
+            } else {
+                // ignore size prefixes
+                if (c == 'h' || c == 'l' || c == 'j' || c == 'z' || c == 't' || c == 'w' || c == 'L' || c == 'I') continue;
+                // Type specifier, perform conversion.
+                if (++valIdx == numArgs) {
+                    debugPrint("%s() - format string contains more conversions than passed arguments (%d): %s",
+                        infoOpcodeName, numArgs - 1, format);
+                }
+                const auto& arg = formatArgs[std::min(valIdx - 1, numArgs - 2)];
+
+                // ctx.arg(valIdx < numArgs ? valIdx : numArgs - 1);
+                if (c == 'S' || c == 'Z') {
+                    c = 's'; // don't allow wide strings
+                }
+                if (c == 's' && !arg.isString() || // don't allow treating non-string values as string pointers
+                    c == 'n') // don't allow "n" specifier
+                {
+                    c = 'd';
+                }
+                newFmt[j++] = c;
+                newFmt[j] = '\0';
+                partLen = arg.isFloat()
+                    ? snprintf(outBuf, bufCount, newFmt, arg.floatValue)
+                    : arg.isInt()    ? snprintf(outBuf, bufCount, newFmt, arg.integerValue)
+                    : arg.isString() ? snprintf(outBuf, bufCount, newFmt,
+                                           programGetString(program, arg.opcode, arg.integerValue))
+                                     : snprintf(outBuf, bufCount, newFmt, "<UNSUPPORTED TYPE>");
+            }
+            outBuf += partLen;
+            bufCount -= partLen;
+            conversion = false;
+            j = 0;
+            if (bufCount <= 0) {
+                break;
+            }
+            continue;
+        }
+        newFmt[j++] = c;
+    }
+    // Copy the remainder of the string.
+    if (bufCount > 0) {
+        newFmt[j] = '\0';
+        // strcpy_s(outBuf, bufCount, newFmt);
+        if (strlen(newFmt) < bufCount) {
+            strcpy(outBuf, newFmt);
+        } else {
+            strncpy(outBuf, newFmt, bufCount - 1);
+            outBuf[bufCount - 1] = '\0'; // Ensure null-termination
+        }
+    }
+
+    delete[] newFmt;
+
+    programStackPushString(program, out);
 }
 
 void sfall_metarule(Program* program, int args)
