67592fa04c3f054080aab07e66572b81cb6644a6
Vasilii Rogin <vasya@rogin.ru>
2025-12-24 11:19:07 +0200

    Allow clearing error text

--- os/web/setErrorState.mjs
+++ os/web/setErrorState.mjs
@@ -1,8 +1,32 @@
+/** @type {Date | null} */
+let lastTimeError = null;
+let onClickInitialized = false;
+
 /**
  * @param {Error} err
  */
 export function setErrorState(err) {
-    // @ts-ignore
-    document.getElementById("error_text").innerText =
-        `\n\n${err?.message}\n${err?.stack}`;
+    const elem = document.getElementById("error_text");
+    if (!elem) {
+        console.warn("No error_text element found");
+        return;
+    }
+    elem.innerText = `\n\n${err?.message}\n${err?.stack}`;
+
+    lastTimeError = new Date();
+    if (!onClickInitialized) {
+        onClickInitialized = true;
+        document.body.addEventListener("click", () => {
+            if (lastTimeError) {
+                const now = new Date();
+                const timeDiff = Math.floor(
+                    (now.getTime() - lastTimeError.getTime()) / 1000,
+                );
+                if (timeDiff > 2) {
+                    lastTimeError = null;
+                    elem.innerText = "";
+                }
+            }
+        });
+    }
 }
