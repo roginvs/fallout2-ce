ad70dbd1de1bebc1248b79796479852cc199be01
Vasilii Rogin <vasya@rogin.ru>
2025-12-24 12:36:27 +0200

    Deploy debug version

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -22,22 +22,41 @@ echo ""
 echo ""
 echo ""
 
+echo "=== Debug build ==="
+docker run --rm --user $(id -u):$(id -g) -v $(pwd):$(pwd) -w $(pwd) \
+  emscripten/emsdk:3.1.74 \
+  sh -c 'mkdir -p build.debug && cd build.debug && 
+        export SOURCE_MAP_BASE=https://fallout-nevada.ru/ &&
+        export EM_CACHE=$(pwd)/build.debug/emcache && 
+        emcmake cmake -DCMAKE_BUILD_TYPE="Debug" ../ && 
+        emmake make VERBOSE=1 -j 8'
+
+echo ""
+echo ""
+echo ""
 
 echo "=== Deploy to server (dry run) ==="
-cd build/web
+
 
 RSYNC_ARGS=(
   -avz --progress --info=progress2
   -f '+ *.mjs' -f '+ *.css' -f '+ *.wasm' -f '+ *.wasm.map'
   -f '+ *.ico' -f '+ *.png' -f '+ *.js' -f '+ *.html' -f '+ manifest.json'
-  -f '- *' --checksum ./ root@fallout-nevada.ru:/var/www/html/
+  -f '- *' --checksum ./
 )
-rsync --dry-run "${RSYNC_ARGS[@]}"
+
+echo "=== Release build: ==="
+(cd build/web       && rsync --dry-run "${RSYNC_ARGS[@]}" root@fallout-nevada.ru:/var/www/html/)
+echo "=== Debug build: ==="
+(cd build.debug/web && rsync --dry-run "${RSYNC_ARGS[@]}" root@fallout-nevada.ru:/var/www/htmlbeta/)
 
 read -rp "Proceed with real sync? [y/N] " confirm
 if [[ $confirm =~ ^[Yy]$ ]]; then
   echo "Running real rsync..."
-  rsync "${RSYNC_ARGS[@]}"
+  echo "=== Release build: ==="
+  (cd build/web       && rsync "${RSYNC_ARGS[@]}" root@fallout-nevada.ru:/var/www/html/)
+  echo "=== Debug build: ==="
+  (cd build.debug/web && rsync "${RSYNC_ARGS[@]}" root@fallout-nevada.ru:/var/www/htmlbeta/)
 else
   echo "Aborted."
 fi
